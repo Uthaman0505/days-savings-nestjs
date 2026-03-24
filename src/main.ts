d114443bb401b34cb736dc6e4d37030e01068b31
setia <setia@Mac-mini.local>
2026-03-24 17:15:29 +0800

    fix: minor fix on port changes

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  const PORT = process.env.APP_PORT || process.env.PORT as string
-  await app.listen(PORT, '0.0.0.0', () => { console.log(`${`Your app runs on http://localhost:${PORT}/graphql`}`) });
+  const PORT = process.env.APP_PORT || (process.env.PORT as string);
+  await app.listen(PORT, '0.0.0.0', () => { console.log(`${`Your·app·runs·on·http://localhost:${PORT}/graphql`}`) });
   // await app.listen(PORT, '0.0.0.0', () => { console.log(`${`Your app runs on http://localhost:${PORT}/graphql`}`) }
 }
 
